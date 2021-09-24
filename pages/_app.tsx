3e59f4b6074623f4ac65f1290da77e3ae7fb04ac
jainChetan81 <jain.cj.chetan@gmail.com>
2021-09-24 14:05:23 +0530

    removed unsused imports in _app.tsx and added package-lock.json with fixes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,6 @@
 import "../styles/globals.css";
-import type { ReactElement, ReactNode } from "react";
 import type { AppProps } from "next/app";
-import type { NextPage } from "next";
 
-type NextPageWithLayout = NextPage & {
-	getLayout?: (page: ReactElement) => ReactNode;
-};
-
-type AppPropsWithLayout = AppProps & {
-	Component: NextPageWithLayout;
-};
 function MyApp({ Component, pageProps }: AppProps) {
 	return <Component {...pageProps} />;
 }
