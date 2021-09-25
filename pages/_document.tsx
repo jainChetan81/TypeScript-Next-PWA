0f36a0e05d547e47be09b7c43c0411c46ef608f2
jainChetan81 <jain.cj.chetan@gmail.com>
2021-09-25 17:11:30 +0530

    added imagekit image to every nook and cranny

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,10 +3,10 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 class MyDocument extends Document {
 	render() {
 		return (
-			<Html>
+			<Html lang="en">
 				<Head>
 					<link rel="manifest" href="/manifest.json" />
-					<link rel="apple-touch-icon" href="/favicon.ico" />
+					<link rel="apple-touch-icon" href="/oo" />
 					<meta name="theme-color" content="#fff" />
 					<meta name="msapplication-TileColor" content="#2B5797" />
 					<meta name="msapplication-tap-highlight" content="no" />
@@ -15,23 +15,20 @@ class MyDocument extends Document {
 					<meta name="apple-mobile-web-app-capable" content="yes" />
 					<meta name="apple-mobile-web-app-status-bar-style" content="default" />
 					<meta name="apple-mobile-web-app-title" content="Quiz App" />
-					<link rel="apple-touch-icon" href="favicon.ico" />
-					<link rel="shortcut icon" href="/favicon.ico" />
+					<link rel="apple-touch-icon" href="https://ik.imagekit.io/aqaseg5nkl6/chetan.jpg?tr=w-300,h-300" />
+					<link rel="shortcut icon" href="https://ik.imagekit.io/aqaseg5nkl6/chetan.jpg?tr=w-300,h-300" />
 					<meta name="twitter:card" content="summary" />
 					<meta name="twitter:url" content="https://type-script-next-pwa.vercel.app" />
 					<meta name="twitter:title" content="Quiz App" />
 					<meta name="twitter:description" content="Quiz app by Next js created by Chetan Jain" />
-					<meta name="twitter:image" content="https://type-script-next-pwa.vercel.app/favicon.ic" />
+					<meta name="twitter:image" content="https://ik.imagekit.io/aqaseg5nkl6/chetan.jpg?tr=w-200,h-200" />
 					<meta name="twitter:creator" content="@DavidWShadow" />
 					<meta property="og:type" content="website" />
 					<meta property="og:title" content="Quiz App" />
 					<meta property="og:description" content="Quiz app by Next js created by Chetan Jain" />
 					<meta property="og:site_name" content="Quiz App" />
 					<meta property="og:url" content="https://type-script-next-pwa.vercel.app" />
-					<meta
-						property="og:image"
-						content="https://type-script-next-pwa.vercel.app/icons/apple-touch-icon.png"
-					/>
+					<meta property="og:image" content="https://ik.imagekit.io/aqaseg5nkl6/chetan.jpg?tr=w-300,h-300" />
 				</Head>
 				<body>
 					<Main />
